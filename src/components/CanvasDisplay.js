a18af17e29cfa75f3e077063a2b71c19fdd52a19
github username <github email address>
2019-08-12 16:54:38 -0700

    changed canvas receiver component to only act on received data when game is joined

--- src/components/CanvasDisplay.js
+++ src/components/CanvasDisplay.js
@@ -84,13 +84,14 @@ class CanvasDisplay extends Component {
         received: (data) => {
           // let paths = this.state.paths;
           // paths.push(data);
-          if (data.clear) {
-            this.clearArea()
+          if (this.props.gameJoined) {
+            if (data.clear) {
+              this.clearArea()
+            }
+            else {
+            this.drawLine(data)
+            }
           }
-          else {
-          this.drawLine(data)
-          }
-          // this.setState({ paths })
         },
         create: function(color, strokeWidth, coordinates) {
 
@@ -119,48 +120,18 @@ class CanvasDisplay extends Component {
     // if (this.props.gameWillEnd) {this.transitionOut()}
     return (
       <Transition visible={visible} duration={1000}>
-      <div className="ui small scale visible transition">
+        <div className="ui small scale visible transition">
           <div className="word"><i className="pencil alternate icon drawIcon"></i> {this.props.drawer} is Drawing</div>
-        <canvas
-          id="canvas"
-          width="600"
-          height="500">
-        </canvas>
-
-      </div>
-     </Transition>
+          <canvas
+            id="canvas"
+            width="600"
+            height="500">
+          </canvas>
+        </div>
+      </Transition>
     )
   }
 
 }
 
 export default CanvasDisplay;
-
-
-// <ActionCable
-//   channel={{channel: "CanvasChannel"}}
-//   onReceived={this.handleReceivedPaths}
-// />
-
-// drawLine = (paths) => {
-//
-//   const canvas = document.getElementById('canvas-display');
-//   const ctx = canvas.getContext('2d');
-//   ctx.save();
-//   ctx.lineJoin = "round";
-//   ctx.lineCap = "round";
-//
-//   this.state.paths.forEach(path => {
-//     ctx.lineWidth = path.strokeWidth;
-//     ctx.strokeStyle = path.color;
-//     ctx.beginPath();
-//     const c = path.coords;
-//     ctx.moveTo(c[0], c[1]);
-//     for (let i = 2; i < c.length; i += 2) {
-//       ctx.lineTo(c[i], c[i+1]);
-//     }
-//     ctx.closePath();
-//     ctx.stroke();
-//   })
-//   ctx.restore();
-// };
