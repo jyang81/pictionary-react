e4c39cece47711cfd1e54476cb17df5049b2d3ea
github username <github email address>
2019-08-14 08:32:51 -0700

    remove get profile call on login, added error handling to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,9 +14,6 @@ class Login extends React.Component {
 
     handleSubmit(ev) {
         ev.preventDefault()
-        this.setState({
-            visible: false
-        })
         let username = this.username.current.value
         this.props.loginNewUser(username)
     }
@@ -27,20 +24,26 @@ class Login extends React.Component {
         }) 
     }
 
+    componentWillUnmount() {
+        this.setState({
+            visible: false
+        })
+    }
+
 
     render() {
         const { visible } = this.state
         return (
             <Transition visible={visible} duration={400}>
-            <div className="ui small visible transition">
-                <form className="ui form" onSubmit={this.handleSubmit} >
-                    <div className="field">
-                        <label><h3>Enter Your Username</h3></label>
-                        <input type="text" name="username" placeholder="Username" required ref={this.username}/>
-                    </div>
-                    <button className="ui button" type="submit">Submit</button>
-                </form>
-            </div>
+                <div className="ui small visible transition">
+                    <form className="ui form" onSubmit={this.handleSubmit} >
+                        <div className="field">
+                            <label><h3>Enter Your Username</h3></label>
+                            <input type="text" name="username" placeholder="Username" required ref={this.username}/>
+                        </div>
+                        <button className="ui button" type="submit">Submit</button>
+                    </form>
+                </div>
             </Transition>
         );
     }
